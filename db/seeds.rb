f4f2a07302c43451100b8fc33e92a69bbaf5391b
tanyishu <kaobei_lah@hotmail.com>
2016-03-23 18:21:43 +0800

    add admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+User.create({
+  email: "kaobei_lah@hotmail.com",
+  password: "12345678",
+  password_confirmation: "12345678",
+  forem_admin: true
+})
